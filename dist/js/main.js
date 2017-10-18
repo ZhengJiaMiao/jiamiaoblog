db54e043fb53f153f108bbc8dd6cff0b455e817a
ZhengJiaMiao <1228062494@qq.com>
2017-10-18 08:46:13 +0800

    1018

--- dist/js/main.js
+++
@@ -1,5 +0,0 @@
-alert(1231);
-alert(1231);
-alert(3);
-var a = 10;
-alert(a);
--- gulpfile.js
+++ gulpfile.js
@@ -37,11 +37,10 @@ gulp.task('js', function() {
   return gulp.src('src/js/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
-    .pipe(concat('main.js'))
+    //.pipe(concat('main.js'))
     .pipe(gulp.dest('dist/js'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'))
     .pipe(notify({ message: 'Scripts task complete' }))
 	.pipe(connect.reload());
 });
